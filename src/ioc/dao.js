4a116f99198b8faf090bf8b1f3f0b744c056bf69
IMUDGES-YY <929144090@qq.com>
2017-05-16 14:51:24 +0800

    update

--- src/ioc/dao.js
+++ src/ioc/dao.js
@@ -8,7 +8,7 @@ var ioc = {
         fields : {
             // url : "jdbc:mysql://127.0.0.1:3306/chinese_medicine_manager_system?useUnicode=true&characterEncoding=UTF8",
             // url : "jdbc:oracle:thin:@localhost:1521:epsit",
-            jdbcUrl : 'jdbc:oracle:thin:@127.0.0.1:1521:xe',
+            jdbcUrl : 'jdbc:oracle:thin:@183.175.12.168:1521:xe',
             username : "yy",
             password : "1228",
             testWhileIdle : true,
